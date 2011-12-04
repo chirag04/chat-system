79d46cf30cc87397a2538f4f67f86e63f04183d6
chirag04 <chirag@kodeplay.com>
2011-12-04 22:25:41 +0530

    first commit

---
+++ multiroomchat_server.js
@@ -0,0 +1,60 @@
+var fs = require('fs');
+var url  = require('url');
+var path = "";
+var server = require('http').createServer(function(req, response){
+    path = url.parse(req.url).pathname;
+    if(path=='/admin'){
+	    fs.readFile(__dirname+'/admin.html', function(err, data){
+	    response.writeHead(200, {'Content-Type':'text/html'});
+	    response.write(data);
+	    response.end();
+	    });
+    }else{
+	    fs.readFile(__dirname+'/buyer.html', function(err, data){
+	    response.writeHead(200, {'Content-Type':'text/html'});
+	    response.write(data);
+	    response.end();
+	    });
+    }
+});
+server.listen(8080);
+
+
+var nowjs = require("now");
+var everyone = nowjs.initialize(server);
+
+var admin_id = 0;
+
+nowjs.on('connect', function(){
+  if( path == "/admin") {
+        admin_id = this.user.clientId;
+  } else {
+
+	if(admin_id!=0){
+          username = this.now.name;
+          this.now.room = this.now.clientId;
+          client_id = this.user.clientId; 
+	  nowjs.getGroup(client_id).addUser(admin_id);
+	  nowjs.getGroup(client_id).addUser(client_id);
+          console.log("Joined: " + username + " "+ client_id);
+          nowjs.getClient(admin_id, function () {
+	      this.now.addUser(username,client_id);
+	  });
+	  }else{
+	    this.now.receiveMessage("Admin","We are offline now.Contact later!");
+            this.now.deletechat();
+	 }
+  }
+});
+
+nowjs.on('disconnect', function(){
+  console.log("Left: " + this.now.name);
+});
+
+everyone.now.distributeMessage = function(message,isadmin,room){
+    if(isadmin){
+	nowjs.getGroup(room).now.receiveMessage(this.now.name, message);
+    }else {
+	nowjs.getGroup(this.user.clientId).now.receiveMessage(this.now.name, message);
+    }
+};
